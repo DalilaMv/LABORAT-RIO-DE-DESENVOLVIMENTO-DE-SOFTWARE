4a6e4276353455c466fb458bfadc5262bc36ebc5
annii202 <annalaura.rdg@gmail.com>
2022-03-22 16:55:47 -0300

    Visualizar alunos matriculados na disciplina - Professor

--- implementacao/projeto_java/src/Professor.java
+++ implementacao/projeto_java/src/Professor.java
@@ -9,7 +9,39 @@ public class Professor extends Pessoa {
         return this.disciplinasLecionadas;
     }
 
+    public void VisualizarDisciplinas(int matricula){
+        ConexaoSQLite conexaoSQLite = new ConexaoSQLite();
+        ResultSet resultSet = null;
+        Statement statement = null;
+        String query;
+        conexaoSQLite.conectar();
+        query = "SELECT ALUNO FROM CURSO
+                WHERE PROFESSOR =  ?";
+                statement = conexaoSQLite.criarStatement();
+        try{
+            statement.setString(1, matricula);
+            resultSet = statement.executeQuery(query);
+            int count = 0;
+            while(resultSet.next()) {
+                count += 1;
+            }
 
+            if(!resultSet.next()){
+                System.out.println(" ");
+            }
+
+        }catch (SQLException e) {
+            System.out.println("erro no sql");
+        }finally{
+            try{
+                resultSet.close();
+                statement.close();
+                conexaoSQLite.desconectar();
+            }catch (SQLException ex){
+                System.out.println("algum erro de fechamento");
+            }
+        }
+    }
     @Override
     public boolean fazerLogin(String senha, int matricula) {
         ConexaoSQLite c = new ConexaoSQLite();
