beb8511addbe30b0fa96d61c227630e2e98a22c5
Dalila <dalilamv99@outlook.com>
2022-03-22 18:07:16 -0300

    ajuste função do prof

--- implementacao/projeto_java/src/Professor.java
+++ implementacao/projeto_java/src/Professor.java
@@ -1,4 +1,8 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Professor extends Pessoa {
     private ArrayList<Disciplina> disciplinasLecionadas = new ArrayList<>();
@@ -9,37 +13,80 @@ public class Professor extends Pessoa {
         return this.disciplinasLecionadas;
     }
 
-    public void VisualizarDisciplinas(int matricula){
+    public void FiltroProf(List<Oferta> lista){
         ConexaoSQLite conexaoSQLite = new ConexaoSQLite();
+        boolean conectou = false;
+        conectou = conexaoSQLite.conectar();
+
         ResultSet resultSet = null;
-        Statement statement = null;
-        String query;
-        conexaoSQLite.conectar();
-        query = "SELECT ALUNO FROM CURSO
-                WHERE PROFESSOR =  ?";
-                statement = conexaoSQLite.criarStatement();
-        try{
-            statement.setString(1, matricula);
-            resultSet = statement.executeQuery(query);
+        PreparedStatement preparedStatement = null;
+        for (int l = 0; l<lista.size(); l+=1){
+            String sqlselect = "SELECT * "
+            + " FROM matricula"
+            + " WHERE oferta = ?";
+
+            preparedStatement = conexaoSQLite.criarPreparedStatement(sqlselect);
+
             int count = 0;
-            while(resultSet.next()) {
-                count += 1;
-            }
+            try {
+                preparedStatement.setInt(1, lista.get(l).getOfertaid());
+            
+                resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    while(resultSet.next()){
+                        count += 1;
+                    }
 
-            if(!resultSet.next()){
-                System.out.println(" ");
+                    System.out.println(count+" aluno(s) cadastrado(s) na sua oferta de disciplina de ID"+ lista.get(l).getOfertaid());
+                } else {
+                    System.out.println("nenhum aluno cadastrado na sua oferta de disciplina de ID"+ lista.get(l).getOfertaid());
+                }
+            }catch(SQLException e) {
+                System.out.println(e);
+            }finally{
+                if(conectou){
+                    conexaoSQLite.desconectar();
+                }
             }
+        }
+    };
 
-        }catch (SQLException e) {
-            System.out.println("erro no sql");
+    public void VisualizarDisciplinas(int matricula){
+       
+        ConexaoSQLite conexaoSQLite = new ConexaoSQLite();
+        boolean conectou = false;
+        conectou = conexaoSQLite.conectar();
+
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+
+        String sqlselect = "SELECT * "
+                + " FROM oferta"
+                + " WHERE professor = ?";
+        preparedStatement = conexaoSQLite.criarPreparedStatement(sqlselect);
+
+        List<Oferta> lista = new ArrayList<Oferta>();
+        try {
+        preparedStatement.setInt(1, matricula);
+
+        resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+            while(resultSet.next()){
+                Oferta oferta = new Oferta();
+                oferta.setOfertaid(resultSet.getInt("ofertaid"));
+                lista.add(oferta);
+            }
+        } else {
+            System.out.println("Você não está lecionando nenhuma disciplina");
+        }
+        }catch(SQLException e) {
+            System.out.println(e);
         }finally{
-            try{
-                resultSet.close();
-                statement.close();
+            if(conectou){
                 conexaoSQLite.desconectar();
-            }catch (SQLException ex){
-                System.out.println("algum erro de fechamento");
             }
+            FiltroProf(lista);
         }
     }
     @Override
